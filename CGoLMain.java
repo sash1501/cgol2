8d0f0b32c5f376a74985cbc30c1bfec1aeeb9739
Sascha <mymailissave@gmail.com>
2017-09-16 23:03:25 +0200

    Gemeinsames Projekt angelegt

---
+++ CGoLMain.java
@@ -0,0 +1,25 @@
+
+/**
+ * CGoLMain ist der Ausgangpunkt für das Programm CGoL.
+ * 
+ * @author Sascha
+ * @version 0.1
+ */
+public class CGoLMain
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse CGoLMain
+     */
+    public CGoLMain()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    public static void Main(String[] args){
+
+    }
+}
