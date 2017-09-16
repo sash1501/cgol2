e2104f58d7b74b1917a28c619272c06dc8ae7376
Dominick <dominick2810@aol.com>
2017-09-17 00:19:40 +0200

    Test

--- CGoLWindow.java
+++ CGoLWindow.java
@@ -5,7 +5,10 @@
  * @author (Dominick K.) 
  * @version (Window 0.1)
  */
-public class CGoLWindow
+
+import javax.swing.JFrame;
+
+public class CGoLWindow extends JFrame
 {
     // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
     private int x;
@@ -15,19 +18,7 @@ public class CGoLWindow
      */
     public CGoLWindow()
     {
-        // Instanzvariable initialisieren
-        x = 0;
-    }
-
-    /**
-     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
-     * 
-     * @param  y    ein Beispielparameter für eine Methode
-     * @return        die Summe aus x und y
-     */
-    public int beispielMethode(int y)
-    {
-        // tragen Sie hier den Code ein
-        return x + y;
+      //Create  
+      JFrame frame = new JFrame("Swing");  
     }
 }
