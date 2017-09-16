8eaad711b7d6373f3240b4d495b7dedfe417f328
Sascha <mymailissave@gmail.com>
2017-09-16 23:05:44 +0200

    Test

---
+++ CGoLWorld.java
@@ -0,0 +1,33 @@
+
+/**
+ * Beschreiben Sie hier die Klasse CGoLWorld.
+ * 
+ * @author Sascha
+ * @version 0.1
+ */
+public class CGoLWorld
+{
+    // Instanzvariablen - ersetzen Sie das folgende Beispiel mit Ihren Variablen
+    private int x;
+
+    /**
+     * Konstruktor für Objekte der Klasse CGoLWorld
+     */
+    public CGoLWorld()
+    {
+        // Instanzvariable initialisieren
+        x = 0;
+    }
+
+    /**
+     * Ein Beispiel einer Methode - ersetzen Sie diesen Kommentar mit Ihrem eigenen
+     * 
+     * @param  y    ein Beispielparameter für eine Methode
+     * @return        die Summe aus x und y
+     */
+    public int beispielMethode(int y)
+    {
+        // tragen Sie hier den Code ein
+        return x + y;
+    }
+}
